67c7f35920d392b93b4fbef41ded9d3e92849d96
Joe Beltran <jab102191@gmail.com>
2023-02-23 10:55:10 -0600

    add tests and ability to say  to any input.

--- code.js
+++ code.js
@@ -4,11 +4,5 @@ const helloWorld = function() {
 }
 
 const sayHello = (input) => {
-    if (input === `Alex`){
-        return `Hello, Alex!`
-    } else if (input === `Jane`){
-    return `Hello, Jane!`;
-    } else {
-        return `Hello, Pat!`
-    }
+    return "Hello, " + input + "!";
 };
